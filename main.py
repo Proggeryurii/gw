5fe35dc8317776ace008d00d0cfbefcba8ecebb7
Yurii Kuzmin <yura.kuzmin.19@list.ru>
2024-07-15 20:33:43 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import application.salary as asal
+import application.db.people as adp
+import datetime
+
+
+if __name__ == '__main__':
+    asal.hello()
+    adp.hello2()
+    print(datetime.date.today())
